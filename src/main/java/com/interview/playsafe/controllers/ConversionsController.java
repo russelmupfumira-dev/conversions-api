3672881630468f77afb0957dfacf62a0ea3f1116
Russel Mupfumira <russel@jmr.co.za>
2020-08-05 12:58:38 +0200

    Added conversion controller

--- src/main/java/com/interview/playsafe/controllers/ConversionsController.java
+++ src/main/java/com/interview/playsafe/controllers/ConversionsController.java
@@ -1,10 +1,17 @@
 package com.interview.playsafe.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/conversions")
 public class ConversionsController {
 
+
+    @GetMapping("/ktoc ")
+    public @ResponseBody
+    String greeting() {
+        return "Hello, World";
+    }
 
 
 }
---
+++ src/test/java/com/interview/playsafe/controllers/ConversionsControllerTest.java
@@ -0,0 +1,7 @@
+package com.interview.playsafe.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConversionsControllerTest {
+
+}
